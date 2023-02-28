6edc77dc6674b964fd19d9a8d113c5d2d7a308ed
Sofia Silva <sofiachaves.ps@outlook.com>
2023-02-28 20:42:32 +0000

    Asscroll :peach:

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ASScroll from '@ashthornton/asscroll';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const asscroll = new ASScroll();
+
+window.addEventListener('load', () => {
+    asscroll.enable();
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
